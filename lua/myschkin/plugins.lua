c226b9e5ef088b12c1eda327957a4cf3f690b6c6
myschkin <kenan.henzelin@proton.me>
2024-02-19 20:37:34 +0100

    added surround

--- lua/myschkin/plugins.lua
+++ lua/myschkin/plugins.lua
@@ -33,7 +33,6 @@ require("lazy").setup({
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you have trouble with this installation, refer to the README for telescope-fzf-native.
     build = 'make',
   },
   { "catppuccin/nvim",                             name = "catppuccin", priority = 1000 },
@@ -81,6 +80,14 @@ require("lazy").setup({
     'numToStr/Comment.nvim',
     opts = {},
     lazy = false,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
   },
   {
     'mrcjkb/rustaceanvim',
